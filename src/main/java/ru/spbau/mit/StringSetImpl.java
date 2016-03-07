61788eae55d36994663eef88b8458c1a477bb305
Наталья Ялышева <natalia.yalysheva@gmail.com>
2016-03-07 22:13:02 +0300

    review changes

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,135 +1,84 @@
 package ru.spbau.mit;
 
-import java.util.LinkedList;
-import java.util.ListIterator;
-
 /**
  * Created by Наталья on 22.02.2016.
  */
 public class StringSetImpl implements StringSet {
 
-    private final class Node {
-        private LinkedList<Node> nodes = new LinkedList<>();
-        private boolean id = false;
-        private char value;
+    private static final class Node {
+        private Node[] nodes = new Node[58];
+        private boolean isTerminal = false;
         private int size = 0;
 
-        public Node addNext(char cur) {
-            Node temp = new Node(cur);
-            nodes.add(temp);
+        private Node() {
+        }
+
+        private Node addNext(char cur) {
+            Node temp = new Node();
+            nodes[cur - 'A'] = temp;
             return temp;
         }
 
-        public void setId(boolean id) {
-            this.id = id;
+        private void setTerminal(boolean terminal) {
+            this.isTerminal = terminal;
         }
 
-        public char getValue() {
-            return value;
-        }
-
-        private Node(char cur) {
-            value = cur;
-        }
-
-        public Node findValue(char cur) {
-            if (nodes == null) {
-                return null;
-            }
-            ListIterator<Node> iterator = nodes.listIterator();
-            while (iterator.hasNext()) {
-                Node node = iterator.next();
-                if (node.getValue() == cur) {
-                    return node;
-                }
-            }
-            return null;
+        private Node findValue(char cur) {
+            return nodes[cur-'A'];
         }
     }
 
-    private Node head = new Node('0');
+    private Node head = new Node();
 
     public int size() {
         return head.size;
     }
 
-    public boolean add(String element) {
-        if (!contains(element)) {
-            if (element.equals("")) {
-                head.setId(true);
-                head.size++;
-                return true;
-            }
-            Node current = head;
-            for (int i = 0; i < element.length(); i++) {
-                char symbol = element.charAt(i);
-                Node next = current.findValue(symbol);
-                current.size++;
-                if (next != null) {
-                    current = next;
-                } else {
-                    current = current.addNext(symbol);
-                }
-                if (i == element.length() - 1) {
-                    current.setId(true);
-                    current.size++;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean contains(String element) {
-        if (element.equals("")) {
-            return head.id;
-        }
+    private boolean walk(String element, Integer a, boolean flag) {
         Node current = head;
         for (int i = 0; i < element.length(); i++) {
             char symbol = element.charAt(i);
             Node next = current.findValue(symbol);
-            if (next == null) {
+            current.size += a;
+
+            if (next == null && a == 0) {
                 return false;
             }
-            if (i == element.length() - 1 && next.id) {
-                    return true;
-            } else {
+
+            if (next != null) {
                 current = next;
+            } else {
+                current = current.addNext(symbol);
             }
         }
-        return false;
+
+        if (a == 0) {
+            return current.isTerminal;
+        }
+        current.setTerminal(flag);
+        current.size += a;
+        return true;
+    }
+
+    public boolean add(String element) {
+        if (contains(element)) {
+            return false;
+        }
+        return walk(element, 1, true);
+    }
+
+    public boolean contains(String element) {
+        return walk(element, 0, false);
     }
 
     public boolean remove(String element) {
-        if (contains(element)) {
-            if (element.equals("")) {
-                head.setId(false);
-                head.size--;
-                return true;
-            }
-            Node current = head;
-            for (int i = 0; i < element.length(); i++) {
-                char symbol = element.charAt(i);
-                Node next = current.findValue(symbol);
-                if (next != null) {
-                    current.size--;
-                    if (i == element.length() - 1 && next.id) {
-                        next.setId(false);
-                        next.size--;
-                        return true;
-                    } else {
-                        current = next;
-                    }
-                }
-            }
+        if (!contains(element)) {
+            return false;
         }
-        return false;
+        return walk(element, -1, false);
     }
 
     public int howManyStartsWithPrefix(String prefix) {
-        if (prefix.equals("")) {
-            return head.size;
-        }
         Node current = head;
         for (int i = 0; i < prefix.length(); i++) {
             char symbol = prefix.charAt(i);
@@ -137,12 +86,8 @@ public class StringSetImpl implements StringSet {
             if (next == null) {
                 return 0;
             }
-            if (i == prefix.length() - 1) {
-                return next.size;
-            } else {
-                current = next;
-            }
+            current = next;
         }
-        return 0;
+        return current.size;
     }
 }
