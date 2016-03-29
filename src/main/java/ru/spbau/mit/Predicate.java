cbdfbbd9b26e1f074141bfacf12fdf00e510c271
rbbelka <rbbelka@gmail.com>
2016-03-29 21:05:46 +0300

    static variable order changed

--- src/main/java/ru/spbau/mit/Predicate.java
+++ src/main/java/ru/spbau/mit/Predicate.java
@@ -3,6 +3,18 @@ package ru.spbau.mit;
 
 public abstract class Predicate<T> {
 
+    public static final Predicate ALWAYS_TRUE = new Predicate() {
+        public Boolean apply(Object arg) {
+            return true;
+        }
+    };
+
+    public static final Predicate ALWAYS_FALSE = new Predicate() {
+        public Boolean apply(Object arg) {
+            return false;
+        }
+    };
+
     public abstract Boolean apply(T arg);
 
     public Predicate<T> or(final Predicate<? super T> pred) {
@@ -28,17 +40,5 @@ public abstract class Predicate<T> {
             }
         };
     }
-
-    static final Predicate ALWAYS_TRUE = new Predicate() {
-        public Boolean apply(Object arg) {
-            return true;
-        }
-    };
-
-    static final Predicate ALWAYS_FALSE = new Predicate() {
-        public Boolean apply(Object arg) {
-            return false;
-        }
-    };
 
 }
